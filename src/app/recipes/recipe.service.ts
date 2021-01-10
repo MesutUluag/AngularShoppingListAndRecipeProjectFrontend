7e361111432a15f563285a563cdd7e0dc626b7e6
Mesut Uluag <punk7119@gmail.com>
2021-01-10 10:00:18 +0300

    add feature of sending enrecipe items to shopping-list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,32 @@
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
-
+@Injectable()
 export class RecipeService {
-  recipeSelected= new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter<Recipe>();
 
-  private recipes: Recipe[] = [new Recipe('Recipe-1', 'Test Recipe', 'https://hips.hearstapps.com/delish/assets/17/39/1506456246-delish-healthy-chicken-casserole-1.jpg'),
-    new Recipe('Recipe-2', 'Test Recipe-2', 'https://hips.hearstapps.com/delish/assets/17/39/1506456246-delish-healthy-chicken-casserole-1.jpg')];
+  private recipes: Recipe[] = [new Recipe('Recipe-1',
+    'Tasty Schnitzel',
+    'https://upload.wikimedia.org/wikipedia/commons/a/a7/Schnitzel_sandwich.jpg',
+    [
+      new Ingredient('Meat', 1), new Ingredient('French Fries', 20)
+    ]),
+    new Recipe('Big Fat Burger', 'Big Fat Burger', 'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns',2), new Ingredient('Meat', 1) ]
+    )];
+
+  constructor(private slErvice : ShoppingListService) {
+  }
 
 
   getRecipes(): Recipe[] {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+      this.slErvice.addIngredients(ingredients);
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -8,8 +8,17 @@ export class ShoppingListService {
   getIngredients(): Ingredient[] {
     return this.ingredients.slice();
   }
-  addIngredient(ingredient: Ingredient){
+
+  addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    // for (const ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // }
+    this.ingredients.push(...ingredients);
     this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
