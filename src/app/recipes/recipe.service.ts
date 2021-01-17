13f9c8100a44639cdfe2f600d5108007eb40bac1
Mesut Uluag <punk7119@gmail.com>
2021-01-17 09:50:49 +0300

    replace EventEmitters to Subject and delete unused code

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,10 @@
-import {EventEmitter, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [new Recipe('Recipe-1',
     'Tasty Schnitzel',
@@ -18,7 +17,7 @@ export class RecipeService {
         new Ingredient('Buns',2), new Ingredient('Meat', 1) ]
     )];
 
-  constructor(private slErvice : ShoppingListService) {
+  constructor(private slErvice: ShoppingListService) {
   }
 
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,16 +9,20 @@ import {ShoppingListService} from './shopping-list.service';
   styleUrls: ['./shopping-list.component.css'],
   providers: []
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  subscription: Subscription;
 
   constructor(private slService: ShoppingListService) {
   }
 
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+    this.subscription = this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
       this.ingredients = ingredients;
     });
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
