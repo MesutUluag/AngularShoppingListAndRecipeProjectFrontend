580187cdaf3495fa30aae34c4a06cf31cd10ebf1
EXT02D24367 <mesut.uluag@consultant.turkcell.com.tr>
2021-01-26 15:51:29 +0300

    add resetting form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -33,7 +33,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
@@ -41,10 +41,16 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient(newIngredient);
     }
+    form.reset();
+    this.editMode = false;
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
 }
