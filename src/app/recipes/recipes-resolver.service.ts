2037260d8023c55c430c3f7de73e8e18cda8a61e
Mesut Uluag <punk7119@gmail.com>
2021-01-30 11:26:55 +0300

    add http functionality - add resolver control to fetch data if user dont have any data

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -3,13 +3,21 @@ import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/rou
 import {Recipe} from "./recipe.model";
 import {DataStorageService} from "../shared/data-storage.service";
 import {Observable} from "rxjs";
+import {RecipeService} from "./recipe.service";
 
 @Injectable({providedIn: 'root'})
-export class RecipesResolverService implements Resolve<Recipe[]>{
-  constructor(private dataStorageService: DataStorageService) {
+export class RecipesResolverService implements Resolve<Recipe[]> {
+  constructor(private dataStorageService: DataStorageService,
+              private recipesService: RecipeService) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Recipe[]> | Promise<Recipe[]> | Recipe[] {
-    return this.dataStorageService.fetchRecipes();
+    const recipes = this.recipesService.getRecipes();
+
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
+    }
   }
 }
