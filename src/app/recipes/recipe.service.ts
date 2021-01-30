ddf23387bf0502f571fb2bd739a014d258a87a4a
Mesut Uluag <punk7119@gmail.com>
2021-01-30 11:07:46 +0300

    add http functionality - add pipe to avoid not ingredient object found exception

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,17 +7,18 @@ import {Subject} from "rxjs";
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
+  private recipes: Recipe[] = [];
 
-  private recipes: Recipe[] = [new Recipe('Recipe-1',
-    'Tasty Schnitzel',
-    'https://upload.wikimedia.org/wikipedia/commons/a/a7/Schnitzel_sandwich.jpg',
-    [
-      new Ingredient('Meat', 1), new Ingredient('French Fries', 20)
-    ]),
-    new Recipe('Big Fat Burger', 'Big Fat Burger', 'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
-      [
-        new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
-    )];
+  // private recipes: Recipe[] = [new Recipe('Recipe-1',
+  //   'Tasty Schnitzel',
+  //   'https://upload.wikimedia.org/wikipedia/commons/a/a7/Schnitzel_sandwich.jpg',
+  //   [
+  //     new Ingredient('Meat', 1), new Ingredient('French Fries', 20)
+  //   ]),
+  //   new Recipe('Big Fat Burger', 'Big Fat Burger', 'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+  //     [
+  //       new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+  //   )];
 
   constructor(private slErvice: ShoppingListService) {
   }
