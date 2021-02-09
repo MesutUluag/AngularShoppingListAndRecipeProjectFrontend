5a05acedd7e42a97fcda7f5f674971c9143817db
EXT02D24367 <mesut.uluag@consultant.turkcell.com.tr>
2021-02-09 15:52:42 +0300

    refactor add ingredients method

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -3,6 +3,8 @@ import {Ingredient} from '../../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list.service';
 import {NgForm} from "@angular/forms";
 import {Subscription} from "rxjs";
+import {Store} from "@ngrx/store";
+import * as ShoppingListActions from '../store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -16,7 +18,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   editedItemIndex: number;
   editedItem: Ingredient;
 
-  constructor(private slService: ShoppingListService) {
+  constructor(private slService: ShoppingListService, private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) {
   }
 
   ngOnInit(): void {
@@ -39,7 +41,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     if (this.editMode) {
       this.slService.updateIngredient(this.editedItemIndex, newIngredient);
     } else {
-      this.slService.addIngredient(newIngredient);
+      // this.slService.addIngredient(newIngredient);
+      this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
     }
     form.reset();
     this.editMode = false;
--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,11 +1,12 @@
 import {Action} from '@ngrx/store';
-import {Ingredient} from "../../shared/ingredient.model";
+import {Ingredient} from '../../shared/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 
 
-
-export class AddIngredient implements Action{ // Action is an interface
+export class AddIngredient implements Action { // Action is an interface
   readonly type = ADD_INGREDIENT;
-  payload: Ingredient;
+
+  constructor(public payload: Ingredient) {
+  }
 }
