06f13eeb7eb49755af4308092cb5b1386bfbc6bd
EXT02D24367 <mesut.uluag@consultant.turkcell.com.tr>
2021-02-09 08:23:47 +0300

    add ADD_INGREDIENT actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from './header/header.component';
 import {AppRoutingModule} from './app-routing.module';
 import {SharedModule} from './shared/shared.module';
 import {CoreModule} from './core.module';
+import {StoreModule} from "@ngrx/store";
+import {shoppingListReducer} from "./shopping-list/store/shopping-list.reducer";
 
 @NgModule({
   declarations: [
@@ -16,6 +18,7 @@ import {CoreModule} from './core.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
     SharedModule,
     CoreModule
   ],
---
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -0,0 +1,11 @@
+import {Action} from '@ngrx/store';
+import {Ingredient} from "../../shared/ingredient.model";
+
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+
+
+
+export class AddIngredient implements Action{ // Action is an interface
+  readonly type = ADD_INGREDIENT;
+  payload: Ingredient;
+}
