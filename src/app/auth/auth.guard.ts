d63137872e74cdfc695824b598e5f597ca66d4e2
EXT02D24367 <mesut.uluag@consultant.turkcell.com.tr>
2021-02-12 07:45:39 +0300

    using auth reducer in components part-2

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -3,20 +3,27 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {AuthService} from './auth.service';
 import {map, take} from 'rxjs/operators';
+import {Store} from "@ngrx/store";
+import * as fromApp from "../store/app.reducer";
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, private store: Store<fromApp.AppState>) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean> | boolean {
-    return this.authService.user.pipe(take(1), map(user => { // use take(1) to just take latest user value and unsubscribe
-      const isAuth = !!user; // returns everything not null as true else false
-      if (isAuth) {
-        return true;
-      }
-      return this.router.createUrlTree(['/auth']);
-    }));
+    return this.store.select('auth').pipe(
+      take(1),
+      map(authState => {
+        return authState.user;
+      }),
+      map(user => { // use take(1) to just take latest user value and unsubscribe
+        const isAuth = !!user; // returns everything not null as true else false
+        if (isAuth) {
+          return true;
+        }
+        return this.router.createUrlTree(['/auth']);
+      }));
   }
 
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,9 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {DataStorageService} from '../shared/data-storage.service';
 import {AuthService} from "../auth/auth.service";
 import {Subscription} from "rxjs";
+import {Store} from "@ngrx/store";
+import * as fromApp from "../store/app.reducer";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-header',
@@ -12,11 +15,12 @@ export class HeaderComponent implements OnInit, OnDestroy {
   isAuthenticated = false;
   private userSub: Subscription;
 
-  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService, private store: Store<fromApp.AppState>) {
   }
 
   ngOnInit(): void {
-    this.userSub = this.authService.user.subscribe(user => {
+    this.userSub = this.store.select('auth').pipe(map(authState => authState.user))
+      .subscribe(user => {
       this.isAuthenticated = !!user;
     });
   }
