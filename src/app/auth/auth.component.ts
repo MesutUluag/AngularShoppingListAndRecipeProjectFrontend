ba94c32d876dd694f201fc6be3f15c981f886a63
Mesut Uluag <punk7119@gmail.com>
2021-02-13 10:28:39 +0300

    clean up unused code

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,6 @@
 import {Component, ComponentFactoryResolver, ViewChild, OnDestroy, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {AuthResponseData, AuthService} from './auth.service';
-import {Observable, Subscription} from 'rxjs';
-import {Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 import {AlertComponent} from '../shared/alert/alert.component';
 import {PlaceholderDirective} from '../shared/placeholder/placeholder.directive';
 import {Store} from '@ngrx/store';
@@ -21,8 +19,7 @@ export class AuthComponent implements OnInit, OnDestroy {
   private closeSub: Subscription;
   private storeSub: Subscription;
 
-  constructor(private authService: AuthService,
-              private router: Router,
+  constructor(
               private componentFactoryResolver: ComponentFactoryResolver,
               private store: Store<fromApp.AppState>
   ) {
