9252a4c1579f3d664b68de1f1dbd378bf1b06e8a
Mesut Uluag <punk7119@gmail.com>
2021-02-13 11:16:01 +0300

    refactor setRecipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,11 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
-import {RecipeService} from "../recipe.service";
-import {ActivatedRoute, Router} from "@angular/router";
-import {Subscription} from "rxjs";
+import {RecipeService} from '../recipe.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
+import {Store} from '@ngrx/store';
+import * as fromApp from '../../store/app.reducer';
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-recipe-list',
@@ -13,19 +16,19 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
   subscription: Subscription;
 
-  constructor(private recipeService: RecipeService,
-              private router: Router,
-              private route: ActivatedRoute) {
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private store: Store<fromApp.AppState>) {
   }
 
   ngOnInit(): void {
-    this.subscription = this.recipeService.recipesChanged.subscribe(
+    this.subscription = this.store.select('recipes').pipe(map((recipesState) => recipesState.recipes)).subscribe(
       (recipes: Recipe[]) => {
         this.recipes = recipes;
       }
     );
-    this.recipes = this.recipeService.getRecipes();
   }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
